4bc9f767b6657715d6981a537aff8c34bb7c2df1
QuRyu <Quentin.Liu.0415@gmail.com>
2017-10-21 19:26:33 -0400

    Add the framework for rom

---
+++ pldrom.vhd
@@ -0,0 +1,15 @@
+library ieee;
+library ieee.std_logic_1164.all;
+
+entity ROM is 
+    port (
+        addr : std_logic_vector (3 downto 0);
+        data : std_logic_vector (9 downto 0)
+    );
+end entity;
+
+architecture rlt of ROM is 
+
+begin
+
+end rlt;
