5f323a749490acab7796859803e818e47f0875cf
QuRyu <Quentin.Liu.0415@gmail.com>
2017-10-23 20:15:34 -0400

    final version of command

--- assembler/src/Command.hs
+++ assembler/src/Command.hs
@@ -1,5 +1,6 @@
 module Command (Command, 
-                command)
+                command,
+                converter)
        where
 
 import Data.Tuple (swap)
@@ -14,13 +15,13 @@ type Dst  = String
 type Op   = String
 
 
-opEncod = [("Add", 000), ("Sub", 001), ("Shift_L", 010), ("Shift_R", 011), 
-           ("Xor", 100), ("And", 101), ("(Rotate_L", 110), ("Rotate_R", 111)]
-srcMEncod = [("ACC", 00), ("LR", 01), ("LR", 10), ("Fill", 11)]
-dstMEncod = [("ACC", 00), ("LR", 01), ("ACC", 10), ("ACC", 11)]
-srcAEncod = [("ACC", 00), ("LR", 01), ("IR", 10), ("Fill", 11)]
-dstAEncod = [("ACC", 0), ("LR", 1)]
-srcBEncod = [("ACC", 0), ("LR", 1)]
+opEncod = [("Add", "000"), ("Sub", "001"), ("Shift_L", "010"), ("Shift_R", "011"), 
+           ("Xor", "100"), ("And", "101"), ("(Rotate_L", "110"), ("Rotate_R", "111")]
+srcMEncod = [("ACC", "00"), ("LR", "01"), ("IR", "10"), ("Fill", "11")]
+dstMEncod = [("ACC", "00"), ("LR", "01"), ("ACC_L", "10"), ("ACC_H", "11")]
+srcAEncod = [("ACC", "00"), ("LR", "01"), ("IR", "10"), ("Fill", "11")]
+dstAEncod = [("ACC", "0"), ("LR", "1")]
+srcBEncod = [("ACC", "0"), ("LR", "1")]
 
 fstList :: [(a, b)] -> [a]
 fstList = map fst 
@@ -39,42 +40,42 @@ data Command = Move   Src Dst Val        -- move instruction
              | Branch Src Addr            -- conditional jump
                 deriving (Eq, Show)
             
-command :: [String] -> Maybe Command 
-command ["Move", src, dst, val] = let x =  elemMaybe src srcMTable >> 
-                                           elemMaybe dst dstMTable >> 
+command :: [String] -> Either String Command 
+command ["Move", src, dst, val] = let x =  elemEither src srcMTable >> 
+                                           elemEither dst dstMTable >> 
                                            isVal     val  
                                   in case x of 
-                                       Just _  -> Just $ Move src dst val
-                                       Nothing -> Nothing
+                                       Right _  -> Right $ Move src dst val
+                                       Left s   -> Left s
 command ["Jump", addr]         = let x = isAddr addr 
                                  in case x of 
-                                         Just _  -> Just $ Jump addr 
-                                         Nothing -> Nothing
-command ["Branch", src, addr]  = let x = elemMaybe src srcBTable >>
+                                      Right _  -> Right $ Jump addr 
+                                      Left s   -> Left s
+command ["Branch", src, addr]  = let x = elemEither src srcBTable >>
                                          isAddr    addr
                                  in case x of 
-                                      Just _  -> Just $ Branch src addr
-                                      Nothing -> Nothing 
-command [op, src, dst, val]    = let x = elemMaybe op  opTable >>
-                                         elemMaybe src srcATable >> 
-                                         elemMaybe dst dstATable >>
+                                      Right _ -> Right $ Branch src addr
+                                      Left s  -> Left s 
+command [op, src, dst, val]    = let x = elemEither op  opTable >>
+                                         elemEither src srcATable >> 
+                                         elemEither dst dstATable >>
                                          isVal     val
                                  in case x of 
-                                      Just _  -> Just $ Arith op src dst val
-                                      Nothing -> Nothing
-command _                      = Nothing 
+                                      Right _  -> Right $ Arith op src dst val
+                                      Left s -> Left s
+command _                      = Left "no command given"
 
                                
 
-elemMaybe :: (Eq a, Foldable t) => a -> t a -> Maybe a
-elemMaybe x xs = if x `elem` xs 
-                    then Just x 
-                    else Nothing 
+elemEither :: (Eq a, Show a, Foldable t) => a -> t a -> Either String a
+elemEither x xs = if x `elem` xs 
+                    then Right x 
+                    else Left $ show x ++ " is not supported"
 
-isVal :: String -> Maybe String 
+isVal :: String -> Either String String 
 isVal s = if length s == 4 && isDigitString s
-          then Just "" 
-          else Nothing
+          then Right ""
+          else Left $ s ++ " is not supported" 
 
 isAddr = isVal
 
@@ -86,8 +87,11 @@ isDigitString = foldl' step True
       step False _ = False 
       step True  x = isDigit x
 
+lookupS :: (Eq a) => a -> [(a, b)] -> b
+lookupS x xs = fromJust $ lookup x xs 
+
 converter :: Command -> String 
-converter (Move src dst val) = "00" ++ fromEnumS dst ++ fromEnumS src ++ val
-converter (Arith op src dst val) = "01" ++ fromEnumS op ++ fromEnumS src ++ fromEnumS dst
+converter (Move src dst val) = "00" ++ lookupS dst dstMEncod ++ lookupS src srcMEncod ++ val
+converter (Arith op src dst val) = "01" ++ lookupS op opEncod ++ lookupS src srcAEncod ++ lookupS dst dstAEncod
 converter (Jump addr)            = "10" ++ "0000" ++ addr
-converter (Branch src addr)      = "11" ++ fromEnumS src ++ "000" ++ addr
+converter (Branch src addr)      = "11" ++ lookupS src srcBEncod ++ "000" ++ addr
